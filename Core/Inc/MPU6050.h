e0d843b674074eedf053c9c874edcd73d155acb9
wddycn <2422581995@qq.com>
2025-02-09 20:49:35 +0800

    Delete Core directory

--- Core/Inc/MPU6050.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MPU6050_H
-#define __MPU6050_H
-
-int MPU6050_DMP_Init(void);
-int  MPU6050_DMP_Get_Data(float *pitch, float *roll, float *yaw,short *gx,short *gy,short *gz,short *ax,short *ay,short *az);
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin);
-
-#endif
